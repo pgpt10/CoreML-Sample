b36f8d8d15d99cbe3e1953db2d1bd41be0f4c3c3
Payal Gupta <payal.gupta@99acres.com>
2017-10-05 15:45:15 +0530

    Initial Commit

---
+++ CoreMLSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreMLSample
+//
+//  Created by Payal Gupta on 05/10/17.
+//  Copyright © 2017 Payal Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
